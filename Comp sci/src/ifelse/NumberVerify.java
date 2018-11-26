a1971bfaeb7395981113b417068c2cd809d1c17a
askewste000 <askewste000@MHSR107-HT52S52.sbm1.springbranchisd.com>
2018-11-26 11:20:39 -0600

    k

--- Comp sci/src/ifelse/NumberVerify.java
+++ Comp sci/src/ifelse/NumberVerify.java
@@ -1,5 +1,18 @@
 package ifelse;
+import static java.lang.System.*;
 
-public class NumberVerify {
-
+public class NumberVerify
+{
+	
+	public static boolean isOdd( int num )
+	{
+		if(num%2!=0)
+		return true;
+	}
+	public static boolean isEven( int num )
+	{
+		if(num%2==0)
+		return true;
+	}
+	
 }
--- Comp sci/src/ifelse/NumberVerifyRunner.java
+++ Comp sci/src/ifelse/NumberVerifyRunner.java
@@ -7,10 +7,15 @@ public class NumberVerifyRunner
 {
 	public static void main ( String[] args )
 	{
+		Scanner keyboard = new Scanner(System.in);
+
+		System.out.print("Enter a Whole number :: ");
+		int num = keyboard.nextInt();
 		//add in input
 		System.out.println("5 is odd :: " + NumberVerify.isOdd(5));
 		System.out.println("5 is even :: " + NumberVerify.isEven(5));
-		
+		System.out.println(num+" is odd :: "+NumberVerify.isOdd(num));
+		System.out.println(num+" is even ::"+NumberVerify.isEven(num));
 		//add in more test cases
 	}
 }
