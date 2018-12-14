0f7e6a7d14c3f02fd5a6ae4b4a13fddcbc3bcc89
Sterlingaskew <44846550+Sterlingaskew@users.noreply.github.com>
2018-12-13 21:19:20 -0600

    Update LetterOrVowel.java

--- Comp sci/src/ifelse/LetterOrVowel.java
+++ Comp sci/src/ifelse/LetterOrVowel.java
@@ -4,30 +4,34 @@ import static java.lang.System.out;
 
 import java.util.Scanner;
 
-public class LetterOrVowel {
+public class LetterOrVowel 
+{
 	public static void main ( String[] args )
 	{
-		Scanner keyboard = new Scanner(System.in);
-		out.print("Enter :: ");
-		String word = keyboard.next();
-		String value = word.toLowerCase();
-		char firstletter= value.charAt(0);
-		if(firstletter>=97 && firstletter<=122) 
-			{
-				if (firstletter== 'a'|firstletter=='e'|firstletter== 'i'|firstletter== 'o'|firstletter== 'u')
-					out.print(word+" - vowel");
-				else
-					out.print(word+" - consonant");
-			}
-		if(firstletter>47 && firstletter<58)
-			out.print(word+" - number");
-		if(firstletter<=97&& firstletter<=122)
+		for(int i =0; i<3;i++) 
 		{
+			Scanner keyboard = new Scanner(System.in);
+			out.print("Enter :: ");
+			String word = keyboard.next();
+			String value = word.toLowerCase();
+			char firstletter= value.charAt(0);
+			if(firstletter>=97 && firstletter<=122) 
+				{
+					if (firstletter== 'a'||firstletter=='e'||firstletter== 'i'||firstletter== 'o'||firstletter== 'u')
+						out.print(word+" - vowel\n");
+					else
+						out.print(word+" - consonant\n");
+				}
 			if(firstletter>47 && firstletter<58)
-				out.print("");
+				out.print(word+" - number\n");
+			if(firstletter>32 && firstletter<48)
+				out.print(word+" - something else\n");
+			if(firstletter>57 && firstletter<65)
+				out.print(word+" - something else\n");
+			if(firstletter>90 && firstletter<97)
+				out.print(word+" - something else\n");
+			if(firstletter>122 && firstletter<127)
+				out.print(word+" - something else\n");
 		}
-		else 
-			out.print(word+" - something else");
-		
 	}
 }
